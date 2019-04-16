227e44ce7281f65d0bf8784e24a5001de80ff822
kukkig <me.kukkig@gmail.com>
2019-04-16 16:50:34 +0700

    Add new changes--functions to campaign-creation component

--- src/app/campaign-creation/campaign-creation.component.ts
+++ src/app/campaign-creation/campaign-creation.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { first } from "rxjs/operators";
+import { AlertService } from "../shared/services/alert.service";
+import { CampaignCreationService } from "../campaign-creation/services/campaign-creation.service";
 
 @Component({
   selector: "app-campaign-creation",
@@ -6,14 +16,57 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./campaign-creation.component.scss"]
 })
 export class CampaignCreationComponent implements OnInit {
+  campaignForm: FormGroup;
+  submitted: boolean = false;
+
   creation_pic = "assets/create-campaign-big.PNG";
+  categories = [
+    { id: 1, category: "Education" },
+    { id: 2, category: "Health & Medical" },
+    { id: 3, category: "Human Services" },
+    { id: 4, category: "Animal & Humane" },
+    { id: 5, category: "Arts & Culture" }
+  ];
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private alertService: AlertService,
+    private campaignCreationService: CampaignCreationService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.campaignForm = this.formBuilder.group({
+      campaign_title: ["", Validators.required],
+      campaign_category: ["", Validators.required],
+      target_funds: ["", Validators.required],
+      campaign_recipient: ["", Validators.required],
+      campaign_deadline: ["", Validators.required],
+      campaign_detail: ["", Validators.required],
+      campaign_objective: ["", Validators.required]
+    });
+  }
 
+  get f() {
+    return this.campaignForm.controls;
+  }
+
+  // Action to be triggered after submitting campaign creation form
   onSubmit() {
-    // ฟังก์ชันนี้จะประกอบไปด้วยรูปแบบการเก็บข้อมูลจากฟอร์มcreate campaignว่าเราจะเก็บอะไรบ้างไว้ในตัวแปรอะไร
-    // แล้วจะส่งออกไปในformatไหน ซึ่ง
+    console.warn(this.campaignForm.value);
+
+    this.campaignCreationService
+      .createCampaign(this.campaignForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.alertService.success("Campaign Creation successful", true);
+          this.router.navigate(["/campaign-completion"]);
+        },
+        error => {
+          this.alertService.error(error);
+          this.submitted = false;
+        }
+      );
   }
 }
--- src/app/campaign-creation/campaign-creation.module.ts
+++ src/app/campaign-creation/campaign-creation.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
-import { CampaignCreationComponent } from './campaign-creation.component';
-import { CampaignCreationRoutingModule } from './campaign-creation.routing';
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { CampaignCreationComponent } from "./campaign-creation.component";
+import { CampaignCreationRoutingModule } from "./campaign-creation.routing";
 
 @NgModule({
   declarations: [CampaignCreationComponent],
-  imports: [SharedModule,
-    CampaignCreationRoutingModule
-  ]
+  imports: [SharedModule, CampaignCreationRoutingModule]
 })
-export class CampaignCreationModule { }
+export class CampaignCreationModule {}
