5f0045be49616effacbf9ab356c68309996a82b1
kukkig <me.kukkig@gmail.com>
2019-04-16 16:52:24 +0700

    Delete unwanted module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,51 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [{ 
-  path: '',
-  loadChildren: 'src/app/home/home.module#HomeModule', 
-  pathMatch: 'full' 
-},
-{ 
-  path: 'login',
-  loadChildren: 'src/app/login/login.module#LoginModule', 
-  pathMatch: 'full' 
-},
-{
-  path: 'donor-dashboard',
-  loadChildren: 'src/app/donor-dashboard/donor-dashboard.module#DonorDashboardModule',
-  pathMatch: 'full'
-},
-{
-  path: 'fundraiser-dashboard',
-  loadChildren: 'src/app/fundraiser-dashboard/fundraiser-dashboard.module#FundraiserDashboardModule',
-  pathMatch: 'full'
-},
-{
-  path: 'campaign-inside',
-  loadChildren: 'src/app/campaign-inside/campaign-inside.module#CampaignInsideModule',
-  pathMatch: 'full'
-},
-{
-  path: 'campaign-creation',
-  loadChildren: 'src/app/campaign-creation/campaign-creation.module#CampaignCreationModule',
-  pathMatch: 'full'
-},
-{
-  path: 'campaign-completion',
-  loadChildren: 'src/app/campaign-completion/campaign-completion.module#CampaignCompletionModule',
-  pathMatch: 'full'
-}];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: "src/app/home/home.module#HomeModule",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    loadChildren: "src/app/login/login.module#LoginModule",
+    pathMatch: "full"
+  },
+  {
+    path: "donor-dashboard",
+    loadChildren:
+      "src/app/donor-dashboard/donor-dashboard.module#DonorDashboardModule",
+    pathMatch: "full"
+  },
+  {
+    path: "fundraiser-dashboard",
+    loadChildren:
+      "src/app/fundraiser-dashboard/fundraiser-dashboard.module#FundraiserDashboardModule",
+    pathMatch: "full"
+  },
+  {
+    path: "campaign-inside",
+    loadChildren:
+      "src/app/campaign-inside/campaign-inside.module#CampaignInsideModule",
+    pathMatch: "full"
+  },
+  {
+    path: "campaign-creation",
+    loadChildren:
+      "src/app/campaign-creation/campaign-creation.module#CampaignCreationModule",
+    pathMatch: "full"
+  },
+  {
+    path: "campaign-completion",
+    loadChildren:
+      "src/app/campaign-completion/campaign-completion.module#CampaignCompletionModule",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { HomeModule } from './home/home.module';
-import { LoginModule } from './login/login.module';
-import { DonorDashboardModule } from './donor-dashboard/donor-dashboard.module';
-import { FundraiserDashboardModule }  from './fundraiser-dashboard/fundraiser-dashboard.module';
-import { CampaignInsideModule } from './campaign-inside/campaign-inside.module';
-import { CampaignCreationModule } from './campaign-creation/campaign-creation.module';
-import { CampaignCompletionModule } from './campaign-completion/campaign-completion.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeModule } from "./home/home.module";
+import { LoginModule } from "./login/login.module";
+import { DonorDashboardModule } from "./donor-dashboard/donor-dashboard.module";
+import { FundraiserDashboardModule } from "./fundraiser-dashboard/fundraiser-dashboard.module";
+import { CampaignInsideModule } from "./campaign-inside/campaign-inside.module";
+import { CampaignCreationModule } from "./campaign-creation/campaign-creation.module";
+import { CampaignCompletionModule } from "./campaign-completion/campaign-completion.module";
 
-import { CampaignCardModule } from './campaign-card/campaign-card.module';
+import { CampaignCardModule } from "./campaign-card/campaign-card.module";
 
-import {ApiService} from "./shared/services/api.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {ReactiveFormsModule} from "@angular/forms";
-import {TokenInterceptor} from "./shared/services/interceptor";
+import { ApiService } from "./shared/services/api.service";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TokenInterceptor } from "./shared/services/interceptor";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
     HttpClientModule,
     HomeModule,
     LoginModule,
@@ -35,9 +34,10 @@ import { AppComponent } from './app.component';
     CampaignCardModule,
     AppRoutingModule
   ],
-  providers: [ApiService, {provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptor,
-    multi : true}],
+  providers: [
+    ApiService,
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
