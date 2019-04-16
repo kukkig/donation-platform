019388bb01df9579acf1483b978c60b08aa17006
kukkig <me.kukkig@gmail.com>
2019-04-16 13:01:10 +0700

    Add new design and markup for campaign completion page

--- src/app/campaign-completion/campaign-completion.component.ts
+++ src/app/campaign-completion/campaign-completion.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DatePipe } from "@angular/common";
 
 @Component({
-  selector: 'app-campaign-completion',
-  templateUrl: './campaign-completion.component.html',
-  styleUrls: ['./campaign-completion.component.scss']
+  selector: "app-campaign-completion",
+  templateUrl: "./campaign-completion.component.html",
+  styleUrls: ["./campaign-completion.component.scss"]
 })
 export class CampaignCompletionComponent implements OnInit {
+  // Class variables
+  campaign_poster = "assets/campaign-poster.PNG";
+  today: number = Date.now();
+  public detail_displayed: boolean;
+  public progress_displayed: boolean;
+  public party_displayed: boolean;
 
-  constructor() { }
+  /** Class constructor */
+  constructor() {}
 
+  /** Life Cycle hook to initialize values */
   ngOnInit() {
+    this.detail_displayed = true;
+    this.progress_displayed = false;
+    this.party_displayed = false;
   }
 
+  /** toggle element visibility */
+  public showDetail(): void {
+    this.detail_displayed = true;
+    this.progress_displayed = false;
+    this.party_displayed = false;
+  }
+
+  public showProgress(): void {
+    this.detail_displayed = false;
+    this.progress_displayed = true;
+    this.party_displayed = false;
+  }
+
+  public showParty(): void {
+    this.detail_displayed = false;
+    this.progress_displayed = false;
+    this.party_displayed = true;
+  }
 }
