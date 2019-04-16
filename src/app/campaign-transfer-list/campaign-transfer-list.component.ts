2788e3265c155b2a8fa51116dfca4b9df92f6b9c
kukkig <me.kukkig@gmail.com>
2019-04-16 12:56:25 +0700

    Add new functions and design to transfer list card

--- src/app/campaign-transfer-list/campaign-transfer-list.component.ts
+++ src/app/campaign-transfer-list/campaign-transfer-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-campaign-transfer-list',
-  templateUrl: './campaign-transfer-list.component.html',
-  styleUrls: ['./campaign-transfer-list.component.scss']
+  selector: "app-campaign-transfer-list",
+  templateUrl: "./campaign-transfer-list.component.html",
+  styleUrls: ["./campaign-transfer-list.component.scss"]
 })
 export class CampaignTransferListComponent implements OnInit {
+  poster = "assets/project.PNG";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
