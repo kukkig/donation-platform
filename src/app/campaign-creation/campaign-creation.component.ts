643b84d9f19f7cf28b01394ec19f2da7c28aefd5
Inc <pichamon01@hotmail.com>
2019-04-18 18:39:03 +0700

    Add functions and getter for sending form data to server

--- src/app/campaign-creation/campaign-creation.component.ts
+++ src/app/campaign-creation/campaign-creation.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, from } from 'rxjs';
+
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { Campaign } from '../_models/campaign.model';
+import {CampaignCreationService} from '../_services/campaign-creation.service';
+
 
 @Component({
   selector: 'app-campaign-creation',
@@ -6,12 +15,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-creation.component.scss']
 })
 export class CampaignCreationComponent implements OnInit {
-
   creation_pic = 'assets/create-campaign-big.PNG';
 
-  constructor() { }
+  campaignForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private campaignService: CampaignCreationService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+      this.campaignForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      target: ['', Validators.required],
+      recipient: ['', Validators.required],
+      deadline: ['', Validators.required],
+      description: ['', Validators.required],
+      objective: ['', Validators.required]
+      });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.campaignForm.controls; }
+
+
+  onSubmit() {
+    console.warn(this.campaignForm.value);
+    this.submitted = true;
+     // stop here if form is invalid
+     if (this.campaignForm.invalid) {
+      return;
+    }
+    this.loading = true;
+        this.campaignService.createCampaign(this.campaignForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    console.log('success');
+                    this.router.navigate(['/campaign-completion']);
+                });
+
+
+  }
 }
